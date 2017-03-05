dac6312971c328be91cd52a218da27e200594be8
bromerori <b.romerori@alumnos.urjc.es>
2017-03-05 14:28:16 +0100

    Add security (First part)

---
+++ backend/Cookcinando/src/main/java/daw/cookcinando/security/WebController.java
@@ -0,0 +1,23 @@
+package daw.cookcinando.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+	
+	@RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+	
+	@RequestMapping("/nuevareceta")
+    public String nuevaReceta() {
+        return "crearReceta";
+    }
+}
