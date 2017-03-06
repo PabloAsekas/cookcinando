de0d9c70654538836698575db6c988e7693b5faa
mariablopez <maria.balleslop94@gmail.com>
2017-03-06 11:45:07 +0100

    add Restaurant in all categories

---
+++ backend/Cookcinando/src/main/java/daw/cookcinando/RestaurantController.java
@@ -0,0 +1,25 @@
+package daw.cookcinando;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import daw.cookcinando.model.Recipe;
+
+@Controller
+public class RestaurantController {
+	
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	
+}
