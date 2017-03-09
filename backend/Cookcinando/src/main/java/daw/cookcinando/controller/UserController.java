c148b1d6df1d00ddb81ed3f9eb25b5f54675e8da
bromerori <b.romerori@alumnos.urjc.es>
2017-03-09 17:44:05 +0100

    A try to add the function of registry

--- backend/Cookcinando/src/main/java/daw/cookcinando/controller/UserController.java
+++ backend/Cookcinando/src/main/java/daw/cookcinando/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import daw.cookcinando.model.User;
 import daw.cookcinando.model.UserBasic;
@@ -21,6 +22,35 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@RequestMapping("/registry")
+	public String registry(Model model, @RequestParam String nick, @RequestParam String email, 
+						   @RequestParam String password, @RequestParam String type){
+		
+		model.addAttribute("nick", nick);
+		model.addAttribute("email", email);
+		model.addAttribute("password", password);
+		model.addAttribute("type", type);
+		
+//		if(type == "individual"){
+//			UserBasic user = new UserBasic("", "", "", "", nick,email,password,"ROLE_BASIC");
+//			System.out.println(user);
+//			userRepository.save(user);
+//		}
+//		else if(type == "empresa"){
+//			UserEnterprise user = new UserEnterprise("", "", "", "", nick,email,password,"ROLE_BASIC","ROLE_ENTERPRISE");
+//			System.out.println(user);
+//			userRepository.save(user);
+//		}
+		
+		System.out.println(type);
+		
+//		User user = new User("", "", "", "", nick,email,password,"ROLE_BASIC","ROLE_ENTERPRISE");
+//		System.out.println(user.getEmail());
+//		userRepository.save(user);
+		
+		return "login";
+	}
 
 	@RequestMapping("/privado/todos-usuarios")
 	public String allUsers(Model model, HttpServletRequest request){
