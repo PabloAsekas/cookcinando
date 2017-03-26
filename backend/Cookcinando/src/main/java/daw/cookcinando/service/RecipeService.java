d1b95af739dc36b13c359b4ab227688df93f1df8
mariablopez <maria.balleslop94@gmail.com>
2017-03-26 23:37:19 +0200

    Recipe in API REST

---
+++ backend/Cookcinando/src/main/java/daw/cookcinando/service/RecipeService.java
@@ -0,0 +1,44 @@
+package daw.cookcinando.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import daw.cookcinando.model.Recipe;
+import daw.cookcinando.repository.RecipeRepository;
+
+@Service
+public class RecipeService {
+
+	@Autowired
+	private RecipeRepository recipeRepository;
+	
+	public Recipe findOne(long id) {
+		return recipeRepository.findOne(id);
+	}
+	
+	public List<Recipe> findAll() {
+		return recipeRepository.findAll();
+	}
+	
+	public void save(Recipe recipe) {
+		recipeRepository.save(recipe);
+	}
+	
+	public void delete(long id) {
+		recipeRepository.delete(id);
+	}
+	
+	public Recipe findByTitle(String title) {
+		Recipe recipe = null;
+		
+		for(Recipe r : recipeRepository.findAll()){
+			if(r.getTitle() == title){
+				recipe = r;
+				break;
+			}
+		}
+		return recipe;
+	}
+}
